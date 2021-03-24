e365028eec3735c833b3be32fbd82299b2aba269
Robert Tilton <robtilton2086@gmail.com>
2021-03-24 18:10:12 -0400

    began building out the models

--- models/Comment.js
+++ models/Comment.js
@@ -2,5 +2,28 @@ const { Sequelize, Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/config');
 
 // TODO: YOUR CODE HERE
+class Comment extends Model{};
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+          }, 
+        body: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+    },
+    {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Comment'
+    }
+)
 
 module.exports = Comment;
--- models/Post.js
+++ models/Post.js
@@ -2,5 +2,32 @@ const { Sequelize, Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/config');
 
 // TODO: YOUR CODE HERE
+class Post extends Model {};
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+          }, 
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        body: {
+            type: DataTypes.STRING,
+            allowNull: false
+          },
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'Post'
+    }
+)
 
 module.exports = Post;
